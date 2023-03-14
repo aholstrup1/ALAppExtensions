e1eea0e6670f352035e297300ec496d17c2fd72a
Alexander Holstrup <aholstrup@microsoft.com>
2023-03-14 10:09:52 +0100

    Updates

--- Build/Scripts/EnlistmentHelperFunctions.psm1
+++ Build/Scripts/EnlistmentHelperFunctions.psm1
@@ -18,3 +18,147 @@ function Get-BuildMode() {
     }
     return 'Default'
 }
+
+
+<#
+.Synopsis
+    Get the value of a key from the BuildConfig.json or AL-GO-Settings file
+.Parameter ConfigType
+    The type of config file to read from. Can be either "BuildConfig" or "AL-GO"
+.Parameter Key
+    The key to read the value from
+#>
+function Get-ConfigValueFromKey() {
+    param(
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("BuildConfig","AL-GO")]
+        [string]$ConfigType = "AL-GO",
+        [Parameter(Mandatory=$true)]
+        [string]$Key
+    )
+
+    if ($ConfigType -eq "BuildConfig") {
+        $ConfigPath = Join-Path (Get-BaseFolder) "Build/BuildConfig.json" -Resolve
+    } else {
+        $ConfigPath = Join-Path (Get-BaseFolder) ".github/AL-Go-Settings.json" -Resolve
+    }
+    $BuildConfig = Get-Content -Path $ConfigPath -Raw | ConvertFrom-Json
+    return $BuildConfig.$Key
+}
+
+<#
+.Synopsis
+    Get the value of a key from the BuildConfig.json or AL-GO-Settings file
+.Parameter ConfigType
+    The type of config file to read from. Can be either "BuildConfig" or "AL-GO"
+.Parameter Key
+    The key to write to
+.Parameter Value
+    The value to set the key to
+#>
+function Set-ConfigValueFromKey() {
+    param(
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("BuildConfig","AL-GO")]
+        [string]$ConfigType = "AL-GO",
+        [Parameter(Mandatory=$true)]
+        [string]$Key,
+        [Parameter(Mandatory=$true)]
+        [string]$Value
+    )
+
+    if ($ConfigType -eq "BuildConfig") {
+        $ConfigPath = Join-Path (Get-BaseFolder) "Build/BuildConfig.json" -Resolve
+    } else {
+        $ConfigPath = Join-Path (Get-BaseFolder) ".github/AL-Go-Settings.json" -Resolve
+    }
+    $BuildConfig = Get-Content -Path $ConfigPath -Raw | ConvertFrom-Json
+    $BuildConfig.$Key = $Value
+    $BuildConfig | ConvertTo-Json -Depth 100 | Set-Content -Path $ConfigPath
+}
+
+
+function Get-AutoSubmissionTopicBranchName()
+{
+    $currentDate = (Get-Date).ToUniversalTime().ToString("yyMMddHHmm")
+
+    return "private/$currentDate"
+}
+
+function New-AutoSubmissionTopicBranch
+{
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='High')]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $BranchName
+    )
+    
+    $currentBranch = RunAndCheck git rev-parse --abbrev-ref HEAD
+    if(($currentBranch -ne $env:RepoBranchName) -and ((Get-NavGitParentBranch -LocalOnly) -ne $env:RepoBranchName))
+    {
+        throw "Current branch expected to be $($env:RepoBranchName) or its child. Actual branch is $currentBranch"
+    }
+
+    RunAndCheck git checkout -b $BranchName | Out-Null
+}
+
+
+<#
+.Synopsis
+    Get the nuget.exe if it doesn't exist
+    Downloads the nuget.exe if it doesn't exist
+.Parameter OutputPath
+    The path where the nuget.exe will be downloaded to
+#>
+function Restore-NugetExe() {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$OutputPath
+    )
+
+    if (!(Test-Path $OutputPath)) {
+        New-Item -ItemType Directory -Path $OutputPath | Out-Null
+    }
+
+    $NugetExePath = Join-Path $OutputPath "nuget.exe"
+    if (!(Test-Path $NugetExePath)) {
+        Write-Host "Downloading nuget.exe"
+        Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile $NugetExePath | Out-Null
+    }
+    return $NugetExePath
+}
+
+<#
+.Synopsis
+    Get a package from Nuget.org
+.Parameter PackageId
+    The package id
+.Parameter Version
+    The package version
+.Parameter OutputPath
+    The path where the package will be downloaded
+#>
+function Get-PackageFromNuget() {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$PackageId,
+        [Parameter(Mandatory=$true)]
+        [string]$Version,
+        [Parameter(Mandatory=$true)]
+        [string]$OutputPath
+    )
+
+    $NugetExePath = Restore-NugetExe -OutputPath $OutputPath
+
+    $NugetPackagePath = Join-Path $OutputPath "$PackageId.$Version"
+    if (!(Test-Path $NugetPackagePath)) {
+        Write-Host "install $PackageId -Version $Version -OutputDirectory $OutputPath -Source https://api.nuget.org/v3/index.json"
+        $NugetExeArguments = "install $PackageId -Version $Version -OutputDirectory $OutputPath -Source https://api.nuget.org/v3/index.json"
+        Invoke-Expression "$NugetExePath $NugetExeArguments" | Out-Null
+    }
+
+    return $NugetPackagePath
+}
+
+Export-ModuleMember -Function *-*
---
+++ Build/Scripts/UpdateBaselineVersion.ps1
@@ -0,0 +1,64 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$Owner = "aholstrup1",
+    [Parameter(Mandatory = $true)]
+    [string]$Repo = "AlAppExtensions",
+    [Parameter(Mandatory = $true)]
+    [string]$TargetBranch = "main",
+    [Parameter(Mandatory = $true)]
+    [string]$Token
+)
+
+Write-Host "Owner: $Owner" -ForegroundColor Magenta
+Write-Host "Repo: $Repo" -ForegroundColor Magenta
+Write-Host "TargetBranch: $TargetBranch" -ForegroundColor Magenta
+
+Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
+
+$latestBaseline = Get-LatestBaselineVersionFromArtifacts
+$currentBaseline = Get-ConfigValueFromKey -Key "baselineVersion" -ConfigType "BuildConfig" 
+
+Write-Host "LatestBaseline: $latestBaseline" -ForegroundColor Magenta
+Write-Host "CurrentBaseline: $currentBaseline" -ForegroundColor Magenta
+
+if ([System.Version] $latestBaseline -gt [System.Version] $currentBaseline) {
+    Write-Host "Updating baseline version from $currentBaseline to $latestBaseline" -ForegroundColor Green
+
+    Set-ConfigValueFromKey -Key "baselineVersion" -Value $latestBaseline -ConfigType "BuildConfig"
+
+    $currentDate = (Get-Date).ToUniversalTime().ToString("yyMMddHHmm")
+    $BranchName = "private/UpdateBaselineVersion-$latestBaseline-$currentDate"
+    git checkout -b $BranchName | Out-Null
+    git push --set-upstream origin $BranchName
+
+    New-GitHubPullRequest -Title "Update baseline version to $latestBaseline" -Head $BranchName -Base $TargetBranch  -Token $Token
+}
+
+
+# This function creates a pull request in Github with the current branch
+function New-GitHubPullRequest {
+    param(
+        [string]$Owner,
+        [string]$Repo,
+        [string]$Title,
+        [string]$Body,
+        [string]$Head,
+        [string]$Base,
+        [string]$Token
+    )
+
+    $uri = "https://api.github.com/repos/$Owner/$Repo/pulls"
+    $headers = @{
+        "Authorization" = "token $Token"
+        "Content-Type"  = "application/json"
+    }
+
+    $body = @{
+        "title" = $Title
+        "body"  = $Body
+        "head"  = $Head
+        "base"  = $Base
+    } | ConvertTo-Json
+
+    Invoke-RestMethod -Uri $uri -Headers $headers -Method Post -Body $body
+}
