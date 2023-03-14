1d2e4272a8900c58833d58590cf6e502807769cf
Alexander Holstrup <aholstrup@microsoft.com>
2023-03-14 11:30:36 +0100

    updates

--- Build/Scripts/UpdateBaselineVersion.ps1
+++ Build/Scripts/UpdateBaselineVersion.ps1
@@ -1,11 +1,11 @@
 Param(
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$Owner = "aholstrup1",
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$Repo = "AlAppExtensions",
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$TargetBranch = "main",
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$Token
 )
 
@@ -13,11 +13,21 @@ Write-Host "Owner: $Owner" -ForegroundColor Magenta
 Write-Host "Repo: $Repo" -ForegroundColor Magenta
 Write-Host "TargetBranch: $TargetBranch" -ForegroundColor Magenta
 
-if (Get-Module -Name 'BcContainerHelper') {
-    Write-Host "Importing BcContainerHelper"
-    Install-Module -Name 'BcContainerHelper'
-    Import-Module -Name 'BcContainerHelper'
-}
+$webClient = New-Object System.Net.WebClient
+$webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumentList ([System.Net.Cache.RequestCacheLevel]::NoCacheNoStore)
+$webClient.Encoding = [System.Text.Encoding]::UTF8
+Write-Host "Downloading GitHub Helper module"
+$GitHubHelperPath = "$([System.IO.Path]::GetTempFileName()).psm1"
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/preview/Github-Helper.psm1', $GitHubHelperPath)
+Write-Host "Downloading AL-Go Helper script"
+$ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
+$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/preview/AL-Go-Helper.ps1', $ALGoHelperPath)
+
+Import-Module $GitHubHelperPath
+. $ALGoHelperPath -local
+
+$baseFolder = GetBaseFolder -folder $PSScriptRoot
+DownloadAndImportBcContainerHelper -baseFolder $baseFolder
 
 Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
 Import-Module $PSScriptRoot\GuardingV2ExtensionsHelper.psm1
@@ -28,17 +38,17 @@ $currentBaseline = Get-ConfigValueFromKey -Key "baselineVersion" -ConfigType "Bu
 Write-Host "LatestBaseline: $latestBaseline" -ForegroundColor Magenta
 Write-Host "CurrentBaseline: $currentBaseline" -ForegroundColor Magenta
 
-if ([System.Version] $latestBaseline -gt [System.Version] $currentBaseline) {
+if ([System.Version] $latestBaseline -ge [System.Version] $currentBaseline) { #TODO: Fix ge
     Write-Host "Updating baseline version from $currentBaseline to $latestBaseline" -ForegroundColor Green
 
     Set-ConfigValueFromKey -Key "baselineVersion" -Value $latestBaseline -ConfigType "BuildConfig"
 
     $currentDate = (Get-Date).ToUniversalTime().ToString("yyMMddHHmm")
     $BranchName = "private/UpdateBaselineVersion-$latestBaseline-$currentDate"
-    git checkout -b $BranchName | Out-Null
-    git push --set-upstream origin $BranchName
+    #git checkout -b $BranchName | Out-Null
+    #git push --set-upstream origin $BranchName
 
-    New-GitHubPullRequest -Title "Update baseline version to $latestBaseline" -Head $BranchName -Base $TargetBranch  -Token $Token
+    #New-GitHubPullRequest -Title "Update baseline version to $latestBaseline" -Head $BranchName -Base $TargetBranch  -Token $Token
 }
 
 
