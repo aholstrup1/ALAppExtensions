69fc1b453f4d5a537ae7d2e69598b56888b2e6e8
Alexander Holstrup <aholstrup@microsoft.com>
2023-03-14 13:18:00 +0100

    Refactor

--- Build/Scripts/UpdateBaselineVersion.ps1
+++ Build/Scripts/UpdateBaselineVersion.ps1
@@ -11,12 +11,6 @@ Param(
     [string]$Token
 )
 
-Write-Host "Owner: $Owner"
-Write-Host "Repo: $Repo"
-Write-Host "TargetBranch: $TargetBranch"
-Write-Host "Actor: $Actor"
-
-
 # Setup Enlistment
 $webClient = New-Object System.Net.WebClient
 $webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumentList ([System.Net.Cache.RequestCacheLevel]::NoCacheNoStore)
@@ -29,6 +23,54 @@ $ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
 $webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/preview/AL-Go-Helper.ps1', $ALGoHelperPath)
 Import-Module $GitHubHelperPath
 . $ALGoHelperPath -local
+# End Enlistment
+
+function Push-AutoSubmissionChange
+(
+    [Parameter(Mandatory=$true)]
+    [string] $BranchName,
+    [string[]] $Files,
+    [string] $CommitMessage
+) 
+{
+    invoke-git add $Files
+    invoke-git commit -m $commitMessage
+    invoke-git push -u origin $BranchName
+}
+
+function Set-GitConfig
+(
+    [Parameter(Mandatory=$true)]
+    [string] $Actor,
+    [Parameter(Mandatory=$true)]
+    [string] $Token
+)
+{
+    invoke-git config --global user.name $Actor
+    invoke-git config --global user.email "$($Actor)@users.noreply.github.com"
+    invoke-git config --global hub.protocol https
+    invoke-git config --global core.autocrlf false
+}
+
+function New-AutoSubmissionTopicBranch
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string] $BranchName,
+        [Parameter(Mandatory=$true)]
+        [string] $SubFolder
+    )
+
+    if ($SubFolder) {
+        $currentDate = (Get-Date).ToUniversalTime().ToString("yyMMddHHmm")
+        $BranchName = "private/$SubFolder/$latestBaseline-$currentDate"
+    }
+    
+    invoke-git checkout -b $BranchName | Out-Null
+
+    return $BranchName
+}
 
 $baseFolder = GetBaseFolder -folder $PSScriptRoot
 DownloadAndImportBcContainerHelper -baseFolder $baseFolder
@@ -42,20 +84,12 @@ $currentBaseline = Get-ConfigValueFromKey -Key "baselineVersion" -ConfigType "Bu
 if ([System.Version] $latestBaseline -gt [System.Version] $currentBaseline) {
     Write-Host "Updating baseline version from $currentBaseline to $latestBaseline"
 
-    $currentDate = (Get-Date).ToUniversalTime().ToString("yyMMddHHmm")
-    $BranchName = "private/UpdateBaselineVersion-$latestBaseline-$currentDate"
-    git checkout -b $BranchName | Out-Null
-
-    git config --global user.email "$Actor@users.noreply.github.com"
-    git config --global user.name "$Actor"
-
     Set-ConfigValueFromKey -Key "baselineVersion" -Value $latestBaseline -ConfigType "BuildConfig"
 
-    git stage .
-    git commit -m "Update baseline version to $latestBaseline"
-    git push --set-upstream origin $BranchName
-
-    New-GitHubPullRequest -Title "Update baseline version to $latestBaseline" -Owner $Owner -Repo $Repo -Head $BranchName -Base $TargetBranch -Token $Token
+    Set-GitConfig -Actor $Actor -Token $Token
+    New-AutoSubmissionTopicBranch -SubFolder "UpdateBaselineVersion"
+    Push-AutoSubmissionChange -BranchName $BranchName -Files @("Build/BuildConfig.json") -CommitMessage $title
+    New-GitHubPullRequest -Title $title -Owner $Owner -Repo $Repo -Head $BranchName -Base $TargetBranch -Token $Token
 } else {
     Write-Host "Current baseline version is already up to date"
 }
