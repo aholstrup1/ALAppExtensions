f77ba1b7cfa63c13783991043558663e7f1005e1
Alexander Holstrup <117829001+aholstrup1@users.noreply.github.com>
2023-06-26 10:32:50 +0200

    Update Set-BuildTag.ps1

--- Build/Scripts/Set-BuildTag.ps1
+++ Build/Scripts/Set-BuildTag.ps1
@@ -3,6 +3,6 @@ Param(
 )
 
 Import-Module $PSScriptRoot\EnlistmentHelperFunctions.psm1
-$buildVersion = Get-BuildVersion -BuildNumber $BuildNumber
+$buildVersion = Get-BuildVersion -BuildNumber $BuildNumber 
 git tag $buildVersion
 git push --tags  
