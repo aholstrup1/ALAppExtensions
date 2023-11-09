7ff73662b8b09da692f4f4b693d390b7c57cdbea
aholstrup1 <aholstrup@microsoft.com>
2023-11-09 19:39:48 +0100

    Do not uninstall sysapp and baseapp

--- Build/Scripts/NewBcContainer.ps1
+++ Build/Scripts/NewBcContainer.ps1
@@ -15,7 +15,11 @@ New-BcContainer @parameters
 $installedApps = Get-BcContainerAppInfo -containerName $containerName -tenantSpecificProperties -sort DependenciesLast
 $installedApps | ForEach-Object {
     Write-Host "Removing $($_.Name)"
-    Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
+    if ($_.Name -notin @("System Application", "Base Application")) {
+        Unpublish-BcContainerApp -containerName $parameters.ContainerName -name $_.Name -unInstall -doNotSaveData -doNotSaveSchema -force
+    } else {
+        Write-Host "Skipping $($_.Name)"
+    }
 }
 
 Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
