7d20a98715cda4c308804383c3861fdc4b08b152
aholstrup1 <aholstrup@microsoft.com>
2023-11-20 13:08:40 +0100

    Only run W1 Tests

--- Build/projects/1st Party Apps Tests (FI)/.AL-Go/CompileAppInBcContainer.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-    [Hashtable] $parameters
-)
-
-$scriptPath = Join-Path $PSScriptRoot "../../../scripts/CompileAppInBcContainer.ps1" -Resolve
-$projectFolder = Join-Path $PSScriptRoot "../../1st Party Apps Tests (FI)"
-
-. $scriptPath -parameters $parameters -currentProjectFolder $projectFolder
